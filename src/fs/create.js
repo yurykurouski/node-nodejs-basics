6219783f03d3916794cfd63e9d6beaf8330dd42d
Yury Kurouski <yury_kurouski@epam.com>
2024-01-24 20:10:25 +0100

    feat: create.js add solution

--- src/fs/create.js
+++ src/fs/create.js
@@ -1,5 +1,30 @@
+import { log } from 'console';
+import fs from 'fs';
+import path from 'path'
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+const FILES_DIR = `${ __dirname }/files`
+
+const FILE_NAME = 'fresh.txt';
+const FILE_CONTENT = 'I am fresh and young'
+
 const create = async () => {
-    // Write your code here 
+    fs.readdir(FILES_DIR, (err, files) => {
+        if (err)
+            throw new Error(err);
+        else {
+            if (files.includes(FILE_NAME)) {
+                throw new Error('FS operation failed');
+            } else {
+                fs.writeFile(`${ FILES_DIR }/${ FILE_NAME }`, FILE_CONTENT, (err) => {
+                    if (err) throw new Error();
+                })
+            }
+        }
+    })
 };
 
 await create();
