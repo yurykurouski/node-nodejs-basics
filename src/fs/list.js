97ff853d6f52dfa22bedf7b3adb1635db83f97aa
Yury Kurouski <yury_kurouski@epam.com>
2024-01-24 21:29:00 +0100

    feat: list.js add solution

--- src/fs/list.js
+++ src/fs/list.js
@@ -1,5 +1,18 @@
+import fs from 'fs';
+import path from 'path';
+import { fileURLToPath } from 'url';
+import { log } from 'console';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+const FILES_DIR = `${ __dirname }/files`;
+
 const list = async () => {
-    // Write your code here 
+    fs.readdir(FILES_DIR, (err, files) => {
+        if (err) throw new Error('FS operation failed');
+        log(files);
+    });
 };
 
 await list();
