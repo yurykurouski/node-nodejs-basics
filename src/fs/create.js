9828c84e44439090f523f01bc25aa2a847edfb97
Yury Kurouski <yury_kurouski@epam.com>
2024-01-24 21:38:46 +0100

    chore: small fix

--- src/fs/create.js
+++ src/fs/create.js
@@ -1,4 +1,3 @@
-import { log } from 'console';
 import fs from 'fs';
 import path from 'path'
 import { fileURLToPath } from 'url';
@@ -6,25 +5,27 @@ import { fileURLToPath } from 'url';
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
-const FILES_DIR = `${ __dirname }/files`
+const FILES_DIR = `${ __dirname }/files`;
 
 const FILE_NAME = 'fresh.txt';
-const FILE_CONTENT = 'I am fresh and young'
+const FILE_CONTENT = 'I am fresh and young';
 
 const create = async () => {
     fs.readdir(FILES_DIR, (err, files) => {
-        if (err)
-            throw new Error(err);
+        if (err) throw new Error(err);
         else {
             if (files.includes(FILE_NAME)) {
                 throw new Error('FS operation failed');
             } else {
-                fs.writeFile(`${ FILES_DIR }/${ FILE_NAME }`, FILE_CONTENT, (err) => {
-                    if (err) throw new Error();
-                })
+                fs.writeFile(
+                    `${ FILES_DIR }/${ FILE_NAME }`,
+                    FILE_CONTENT,
+                    (err) => {
+                        if (err) throw new Error('FS operation failed');
+                    });
             }
         }
-    })
+    });
 };
 
 await create();
