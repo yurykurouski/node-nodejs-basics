9f74065e9f155a9f8f56d39120f44abdced353ee
Yury Kurouski <yury_kurouski@epam.com>
2024-01-24 21:23:48 +0100

    feat: delete.js add solution

--- src/fs/delete.js
+++ src/fs/delete.js
@@ -1,5 +1,18 @@
+import fs from 'fs';
+import path from 'path';
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+const FILES_DIR = `${ __dirname }/files`;
+
 const remove = async () => {
-    // Write your code here 
+    fs.unlink(`${ FILES_DIR }/fileToRemove.txt`, (err) => {
+        if (err) {
+            throw new Error('FS operation failed');
+        }
+    });
 };
 
 await remove();
