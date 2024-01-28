b0cd2d8db9e50311bdbf75f096bc627b8483742f
Yury Kurouski <yury_kurouski@epam.com>
2024-01-28 13:40:06 +0100

    feat: add calculateHash solution

--- src/hash/calcHash.js
+++ src/hash/calcHash.js
@@ -1,5 +1,18 @@
+import { createHash } from "crypto";
+import fs from 'fs';
+import path from 'path';
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
 const calculateHash = async () => {
-    // Write your code here 
+    const reader = fs.createReadStream(`${ __dirname }/files/fileToCalculateHashFor.txt`);
+
+    reader.on('data', (chunk) => {
+        const hash = createHash("sha256").update(chunk).digest("hex");
+        console.log(hash);
+    });
 };
 
 await calculateHash();
