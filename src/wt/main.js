e03b8f9d1a75764d879c6b8ccf59bad6b5f46c57
Yury Kurouski <yury_kurouski@epam.com>
2024-01-28 20:48:59 +0100

    feat: workers add solution

--- src/wt/main.js
+++ src/wt/main.js
@@ -1,5 +1,46 @@
+import os from 'node:os';
+import { Worker } from 'worker_threads'
+import path from 'path';
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+const numCPUs = os.availableParallelism();
+const workers = [];
+
 const performCalculations = async () => {
-    // Write your code here
+    for (let i = 0; i < numCPUs; i++) {
+        const worker = new Worker(path.join(__dirname, 'worker.js'), { workerData: 10 + i });
+
+        worker.postMessage({ workerData: 10 + i });
+
+        workers.push({ status: 'pending' });
+
+        worker.on('message', message => {
+            workers[i] = {
+                status: 'resolved',
+                data: message
+            };
+
+            checkAllReceived();
+        });
+
+        worker.on('error', () => {
+            workers[i] = {
+                status: 'error',
+                data: null
+            };
+
+            checkAllReceived();
+        });
+    }
 };
+
+const checkAllReceived = () => {
+    if (workers.every(worker => worker.status !== 'pending')) {
+        console.log(workers);
+    }
+}
 
 await performCalculations();
--- src/wt/worker.js
+++ src/wt/worker.js
@@ -1,8 +1,13 @@
-// n should be received from main thread
+import { parentPort } from 'worker_threads'
+
 const nthFibonacci = (n) => n < 2 ? n : nthFibonacci(n - 1) + nthFibonacci(n - 2);
 
 const sendResult = () => {
-    // This function sends result of nthFibonacci computations to main thread
+    parentPort.on('message', ({ workerData }) => {
+        const data = nthFibonacci(workerData);
+
+        parentPort.postMessage(data);
+    });
 };
 
 sendResult();
